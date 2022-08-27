4b47a69462627e8f2450a8004acb32e593faae79
Lee Hwan Ho <ghksgh45@gmail.com>
2022-08-27 13:12:52 +0900

    Commit lecture #5.4
    Install view engines(pug) worked in express.js
    Coding base html code with pug
    Learning how to pass variables to views from controller
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/controllers/videoController.js
    	modified:   src/server.js
    	new file:   src/views/base.pug
    	new file:   src/views/home.pug
    	new file:   src/views/partials/footer.pug
    	new file:   src/views/watch.pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch Video");
+export const trending = (req, res) => res.render("home",{pageTitle : "HOME"});
+export const see = (req, res) => res.render("watch", {pageTitle : "WATCH"});
 export const edit = (req, res) => res.send("Edit Video");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
