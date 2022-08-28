0e388ca3fcd25215513e9f83d63ca1f822fe905f
Lee Hwan Ho <ghksgh45@gmail.com>
2022-08-28 19:58:09 +0900

    Commit lecture #5.10
    Learning about mixins concept in pug name of view engine
    
     Changes to be committed:
    	modified:   src/controllers/videoController.js
    	modified:   src/routers/globalRouter.js
    	modified:   src/views/base.pug
    	modified:   src/views/home.pug
    	new file:   src/views/mixins/video.pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,43 @@
-export const trending = (req, res) => res.render("home",{pageTitle : "HOME"});
+const fakeUser = {
+    username : "kee",
+    isLogged : true
+}
+const videos = [
+    {
+        title:"test1",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 month ago",
+        views : 763,
+        id : 1
+    },
+    {
+        title:"test1",
+        rating : 2,
+        comments : 23,
+        createdAt : "3 month ago",
+        views : 763,
+        id : 2
+    },
+    {
+        title:"test3",
+        rating : 3,
+        comments : 22,
+        createdAt : "4 month ago",
+        views : 763,
+        id : 3
+    },
+    {
+        title:"test4",
+        rating : 1,
+        comments : 21,
+        createdAt : "5 month ago",
+        views : 763,
+        id : 4
+    }
+]
+
+export const trending = (req, res) => res.render("home",{pageTitle : "HOME", fakeUser, videos});
 export const see = (req, res) => res.render("watch", {pageTitle : "WATCH"});
 export const edit = (req, res) => res.send("Edit Video");
 export const search = (req, res) => res.send("Search Video");
